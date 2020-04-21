a2963b59e003e1682d36b3671b45d49399571b10
Mathieu Keller <mathieukeller@localhost.localdomain>
2020-04-21 18:21:31 +0200

    configure webpack and add webpack-dev-server with hotreload

--- src/frontend/webpack.config.js
+++ src/frontend/webpack.config.js
@@ -1,13 +1,74 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "ts-loader"
+    entry: './src/index.tsx',
+    mode: 'production',
+    devtool: 'inline-source-map',//todo only in dev mode
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: [/.*\.test.*/, /node_modules/],
+                use: {
+                    loader: "ts-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude: [/node_modules/],
+                use: ['style-loader', "css-loader"]
+            },
+            {
+                loader: require.resolve('file-loader'),
+                exclude: [/\.(css|js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
+                options: {
+                    name: 'static/media/[name].[hash:8].[ext]',
+                },
+            },
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin(
+            Object.assign(
+                {},
+                {
+                    inject: true,
+                    template: "./public/index.html",
+                }, {//todo only in prod
+                    minify: {
+                        removeComments: true,
+                        collapseWhitespace: true,
+                        removeRedundantAttributes: true,
+                        useShortDoctype: true,
+                        removeEmptyAttributes: true,
+                        removeStyleLinkTypeAttributes: true,
+                        keepClosingSlash: true,
+                        minifyJS: true,
+                        minifyCSS: true,
+                        minifyURLs: true,
+                    },
+                }
+            )
+        )],
+    devServer: {
+        port: 3001,
+        inline: true,
+        hot: true,
+        contentBase: ['./src', './public'],
+        watchContentBase: true,
+        proxy: {
+            "**": {
+                changeOrigin: true,
+                target: "localhost:8080"
+            }
         }
-      }
-    ]
-  }
+    }
 };
