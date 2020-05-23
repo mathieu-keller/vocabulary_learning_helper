f4a7fda5476c536e9ff479e65375d70aedd879d8
Mathieu Keller <MathieuKeller@gmx.de>
2020-05-23 16:12:23 +0200

    fix eslint. add travis

--- src/frontend/src/tests/reducers/testVocabularies.test.ts
+++ src/frontend/src/tests/reducers/testVocabularies.test.ts
@@ -8,13 +8,13 @@ describe("test test reducer", () => {
                 {id: '1', listId: '2', values: [{key: 'German', value: 'Hallo'}, {key: 'English', value: 'hello'}]},
                 {id: '3', listId: '4', values: [{key: 'German', value: 'Danke'}, {key: 'English', value: 'thanks'}]}
             ], front: 'German', back: 'English'
-        }
+        };
         const state = testVocabularies({...initialState},
             {type: testVocabularyActions.SET_TEST_DATA, payload: testVocabularyData});
         const expectedState = {
             ...initialState, vocabularies: testVocabularyData.vocabularies,
             front: testVocabularyData.front, back: testVocabularyData.back
-        }
+        };
         expect(state).toStrictEqual(expectedState);
     });
-})
+});
--- src/frontend/src/tests/reducers/user.test.ts
+++ src/frontend/src/tests/reducers/user.test.ts
@@ -4,18 +4,18 @@ import {userActions} from "../../actions/actions.types";
 describe("test user reducer", () => {
     it("set login", async () => {
         const state = user({...initialState, isLogin: false}, {type: userActions.LOGIN});
-        const expectedState = {...initialState, isLogin: true}
+        const expectedState = {...initialState, isLogin: true};
         expect(state).toStrictEqual(expectedState);
     });
     it("set logout", async () => {
         const categories = [
             {id: 'id', name: 'japanese', columns: ["japanese", "kanji", "german"], owner: 'mike miller'},
             {id: 'id', name: 'english', columns: ["english", "german"], owner: 'mike miller'}
-        ]
+        ];
         const vocabularyLists = [
             {id: 'id', name: "A1", categoryId: 'catId'},
             {id: 'id', name: "A2", categoryId: 'catId'}
-        ]
+        ];
         const selectedCategory = {id: 'id', name: 'english', columns: ["english", "german"], owner: 'mike miller'};
         const state = user({
             ...initialState,
@@ -24,25 +24,25 @@ describe("test user reducer", () => {
             vocabularyLists: vocabularyLists,
             selectedCategory: selectedCategory
         }, {type: userActions.LOGOUT});
-        const expectedState = {...initialState, isLogin: false}
+        const expectedState = {...initialState, isLogin: false};
         expect(state).toStrictEqual(expectedState);
     });
     it("store categories", async () => {
         const categories = [
             {id: 'id', name: 'japanese', columns: ["japanese", "kanji", "german"], owner: 'mike miller'},
             {id: 'id', name: 'english', columns: ["english", "german"], owner: 'mike miller'}
-        ]
+        ];
         const state = user(initialState, {type: userActions.STORE_CATEGORIES, payload: categories});
-        const expectedState = {...initialState, categories: categories}
+        const expectedState = {...initialState, categories: categories};
         expect(state).toStrictEqual(expectedState);
     });
     it("store vocabulary lists", async () => {
         const vocabularyLists = [
             {id: 'id', name: "A1", categoryId: 'catId'},
             {id: 'id', name: "A2", categoryId: 'catId'}
-        ]
+        ];
         const state = user(initialState, {type: userActions.STORE_VOCABULARY_LISTS, payload: vocabularyLists});
-        const expectedState = {...initialState, vocabularyLists: vocabularyLists}
+        const expectedState = {...initialState, vocabularyLists: vocabularyLists};
         expect(state).toStrictEqual(expectedState);
     });
     it("set selected category", async () => {
@@ -51,7 +51,7 @@ describe("test user reducer", () => {
             type: userActions.SET_SELECTED_CATEGORY,
             payload: selectedCategory
         });
-        const expectedState = {...initialState, selectedCategory: selectedCategory}
+        const expectedState = {...initialState, selectedCategory: selectedCategory};
         expect(state).toStrictEqual(expectedState);
     });
-})
+});
