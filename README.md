75f8e9955e6110ad7ff284360715b040accaf13a
mathieu <mathieukeller@gmx.de>
2020-06-26 22:17:46 +0200

    add readme

---
+++ README.md
@@ -0,0 +1,28 @@
+#Vocabulary trainer
+This app tests your vocabulary with a vocabulary test.
+This app will also store and manage all your vocabulary.
+
+[live demo](https://trainer.mathieukeller.de)
+
+* user: testuser
+* password: start
+
+#####How is the container executed?
+Pull the Docker image or create one.
+
+For docker pull: docker pull afrima/vocabulary_trainer
+
+Run the Docker image with the following environment variables:
+
+| variables | for what?                                                      |
+|----------|--------------------------------------------------------------|
+| GIN_MODE  | right-aligned                                                 |
+| dbUser    | dbUser                                                        |
+| dbPassword| dbPassword                                                    |
+| dbAddress | mongo DB url                                                  |
+| tokenKey  | key for the JWT signature                                     |
+| cookieKey | Secret key to enter/decode the cookie                         |
+| Host      | for which host is the cookie?                                 |
+| secure    | says if the cookie should only be used for a secure connection|
+| Port      | on which port it is running                                   |
+
--- src/frontend/src/containers/category/CategoryView.tsx
+++ src/frontend/src/containers/category/CategoryView.tsx
@@ -9,66 +9,66 @@ import {AppStore} from "../../store/store.types";
 import {userActionFunctions} from "../../actions/user";
 
 export type Category = {
-    readonly id?: string;
-    readonly name: string;
-    readonly columns: string[];
-    readonly owner: string;
+  readonly id?: string;
+  readonly name: string;
+  readonly columns: string[];
+  readonly owner: string;
 }
 
 const CategoryView = (props: RouteComponentProps): JSX.Element => {
-    const emptyCategory = {name: '', columns: ['', ''], owner: ''};
-    const [editCategory, setEditCategory] = useState<Category>(emptyCategory);
-    const [showModal, setShowModal] = useState<boolean>(false);
-    const categories = useSelector((store: AppStore) => store.user.categories);
-    const dispatch = useDispatch();
+  const emptyCategory = {name: '', columns: ['', ''], owner: ''};
+  const [editCategory, setEditCategory] = useState<Category>(emptyCategory);
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const categories = useSelector((store: AppStore) => store.user.categories);
+  const dispatch = useDispatch();
 
-    const onClose = (): void => {
-        setEditCategory(emptyCategory);
-        setShowModal(false);
-    };
+  const onClose = (): void => {
+    setEditCategory(emptyCategory);
+    setShowModal(false);
+  };
 
-    const onSave = async (data: Category): Promise<void> => {
-        post<Category, Category>('/category', data)
-            .then(r => {
-                if (typeof r !== 'string') {
-                    dispatch(userActionFunctions.storeCategories([...categories, r]));
-                    onClose();
-                }
-            });
-    };
-
-    const onClick = (data: Category): void => {
-        const {pathname} = props.location;
-        dispatch(userActionFunctions.setSelectedCategory(data));
-        if (pathname !== '/category') {
-            props.history.push(`${pathname}/${data.owner}/${data.name}`);
+  const onSave = async (data: Category): Promise<void> => {
+    post<Category, Category>('/category', data)
+      .then(r => {
+        if (typeof r !== 'string') {
+          dispatch(userActionFunctions.storeCategories([...categories, r]));
+          onClose();
         }
-    };
+      });
+  };
 
-    const deleteHandler = (id?: string): void => {
-        if (id) {
-            deleteCall<null, string>(`/category/${id}`, null)
-                .then(resId => dispatch(userActionFunctions.removeCategory(resId)));
-        }
-    };
+  const onClick = (data: Category): void => {
+    const {pathname} = props.location;
+    dispatch(userActionFunctions.setSelectedCategory(data));
+    if (pathname !== '/category') {
+      props.history.push(`${pathname}/${data.owner}/${data.name}`);
+    }
+  };
 
-    return (<>
-            {showModal ? <CategoryEditModal
-                saveHandler={onSave}
-                editData={editCategory}
-                cancelHandler={onClose}
-                modalClosed={onClose}/> : null}
-            <Paper>
-                <CardGrid<Category>
-                    deleteHandler={deleteHandler}
-                    onClick={onClick}
-                    title='Choose Category:'
-                    cards={categories}
-                    addAction={() => setShowModal(true)}
-                    setEditHandler={(d: Category) => console.log(d)}/>
-            </Paper>
-        </>
-    );
+  const deleteHandler = (id?: string): void => {
+    if (id) {
+      deleteCall<null, string>(`/category/${id}`, null)
+        .then(resId => dispatch(userActionFunctions.removeCategory(resId)));
+    }
+  };
+
+  return (<>
+      {showModal ? <CategoryEditModal
+        saveHandler={onSave}
+        editData={editCategory}
+        cancelHandler={onClose}
+        modalClosed={onClose}/> : null}
+      <Paper>
+        <CardGrid<Category>
+          deleteHandler={deleteHandler}
+          onClick={onClick}
+          title='Choose Category:'
+          cards={categories}
+          addAction={() => setShowModal(true)}
+          setEditHandler={(d: Category) => console.log(d)}/>
+      </Paper>
+    </>
+  );
 };
 
 export default CategoryView;
